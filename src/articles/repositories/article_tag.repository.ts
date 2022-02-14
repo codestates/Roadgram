525025deb86b3523e6a1c4d9899a95bafde975ff
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-02-14 11:34:39 +0900

    Revert "first commit"

--- src/articles/repositories/article_tag.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { ArticleToTag } from "../entities/article_tag.entity";
-
-@EntityRepository(ArticleToTag)
-export class ArticleToTagRepository extends Repository<ArticleToTag>{
-
-}
