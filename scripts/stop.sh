01e3f70ac740dc958c413e4cc98391b22e20c691
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-02-14 14:24:23 +0900

    Fix scripts

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/Roadgram/server
-pm2 stop /dist/main.js 2> /dev/null || true
-pm2 delete /dist/main.js 2> /dev/null || true
+pm2 stop /dist/src/main.js 2> /dev/null || true
+pm2 delete /dist/src/main.js 2> /dev/null || true
