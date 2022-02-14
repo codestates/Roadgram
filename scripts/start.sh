a03b2b81839155f9880cda605ed57e5f53011d58
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-02-14 14:21:32 +0900

    Fix start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/Roadgram/server
+npm run build
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
