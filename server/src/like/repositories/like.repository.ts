0b8b7487c46f243918cfe1336a3688aa413ed928
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-02-15 17:40:47 +0900

    Fix 중복폴더 삭제

--- server/src/like/repositories/like.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Like } from "../entities/like.entity";
-
-@EntityRepository(Like)
-export class LikeRepository extends Repository<Like>{
-    
-}
