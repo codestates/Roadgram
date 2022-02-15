3a9b04f5ff0a1465009d6eb4685d3618a71fc5b9
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2022-02-15 17:20:12 +0900

    Fix article function

--- server/src/articles/repositories/tag.repository.ts
+++ server/src/articles/repositories/tag.repository.ts
@@ -3,5 +3,13 @@ import { Tag } from "../entities/tag.entity";
 
 @EntityRepository(Tag)
 export class TagRepository extends Repository<Tag>{
+  async findTagName(tagname) {
+    const reuslt = await this.find({tag_name: tagname})
+    return reuslt;
+  }
 
+  async createTag(tagname) {
+    const result = await this.save({tag_name: tagname});
+    return result;
+  }
 }
