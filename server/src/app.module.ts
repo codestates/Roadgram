4a0188189cf6d0345227f61a3461a8f14e90380d
alsrlqor <skyup9393@gmail.com>
2022-02-15 17:21:40 +0900

    Likes name update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesModule } from './articles/articles.module';
 import { SearchModule } from './search/search.module';
-import { CommentsModule } from './comments/comments.module';
-import { LikeModule } from './like/like.module';
+import { CommentsModule } from './comment/comment.module';
+import { LikesModule } from './likes/likes.module';
 import { FollowModule } from './follow/follow.module';
 import { typeORMConfig } from 'config/typeorm.config';
 
@@ -15,8 +15,8 @@ import { typeORMConfig } from 'config/typeorm.config';
     ArticlesModule,
     SearchModule,
     CommentsModule,
-    LikeModule,
+    LikesModule,
     FollowModule
   ],
 })
-export class AppModule { }
+export class AppModule {}
