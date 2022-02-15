18abf738b2469a41743997f21ae5ec456918d532
박종범 <82630476+pjb642@users.noreply.github.com>
2022-02-15 17:42:25 +0900

    create client folder

---
+++ client/src/components/Navigator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../logo.png';
+
+function Navigator() {
+  return (
+    <div>
+      <img alt="logoImg" src={logo} />
+    </div>
+  );
+}
+
+export default Navigator;
