070ba17fb6eed4ee5ecb6fd9de720ca34615c31c
박종범 <82630476+pjb642@users.noreply.github.com>
2022-02-16 17:33:56 +0900

    Add navigator component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Footer from 'components/Footer';
 import Navigator from './components/Navigator';
-// import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div>
       <Navigator />
-      <div>Test</div>
       <Footer />
     </div>
   );
