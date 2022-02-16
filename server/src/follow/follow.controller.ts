e83b27c163dba9f7d52a4cf1ea905bb7a8a3ab8b
alsrlqor <skyup9393@gmail.com>
2022-02-16 17:29:57 +0900

    Follow API start

--- server/src/follow/follow.controller.ts
+++ server/src/follow/follow.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { FollowDto } from './dto/follow.dto';
+import { FollowService } from './follow.service';
 
 @Controller('follow')
-export class FollowController {}
+export class FollowController {
+    constructor(private followService: FollowService) {}
+
+    // @Post()
+    // @HttpCode(200)
+    // followUnFollow(
+    //   //@Headers('authorization')
+    //   @Body() followDto: FollowDto,
+    // ): Promise<object> {
+    //   return this.followService.followUnfollow(followDto);
+    // }
+}
--- server/src/follow/follow.service.ts
+++ server/src/follow/follow.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { fips } from 'crypto';
+import { UserRepository } from 'src/users/repositories/user.repository';
+import { FollowDto } from './dto/follow.dto';
+import { Follow } from './entities/follow.entity';
+import { FollowRepository } from './repositories/follow.repository';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+  constructor(
+    @InjectRepository(FollowRepository)
+    private followRepository: FollowRepository,
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository
+  ) {}
+
+//   async followUnfollow(followDto: FollowDto): Promise<object> {
+//     const { user, followingUserId } = followDto;
+//     const followedOrNot = await this.followRepository.followedOrNot(followDto);
+
+//     if (followedOrNot === undefined) {
+//       const result = await this.followRepository.follow(followDto);
+//       const followedUserInfo = await this.userRepository.followerIncrement(followingUserId);
+//       await this.userRepository.followingIncrement(user);
+//       return {
+//         data: followedUserInfo,
+//         message: result
+//       }
+//     } else {
+//       const result = await this.followRepository.unfollow(followDto);
+//       const followedUserInfo = await this.userRepository.followerDecrement(followingUserId);
+//       await this.userRepository.followingDecrement(user);
+//       return {
+//         data: followedUserInfo,
+//         message: result
+//       }
+//     }
+//   }
+}
