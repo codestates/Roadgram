d153a484e6ee3ec88f70f3d201698754cb45ee37
박종범 <82630476+pjb642@users.noreply.github.com>
2022-02-17 05:23:02 +0900

    Add client folder

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Navigator from './components/Navigator'
+
+function App() {
+  return (
+    <div>
+      <Navigator />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
