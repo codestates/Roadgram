711d53eb2aa32a6ffa1c76b08eb164fd0c00ce6e
alsrlqor <skyup9393@gmail.com>
2022-02-18 17:30:07 +0900

    response update

---
+++ server/src/comments/repositories/comments.repository.ts
@@ -0,0 +1,21 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Comments } from '../entities/comments.entity';
+import { CreateCommentDto, ModifyCommentDto } from '../dto/comments.dto';
+
+@EntityRepository(Comments)
+export class CommentRepository extends Repository<Comments> {
+  async createComment(createCommentDto: CreateCommentDto): Promise<object> {
+    const { user, articleId, comment } = createCommentDto;
+
+    const newComment = this.create({
+      userId: user,
+      articleId: articleId,
+      comment,
+    });
+    await this.save(newComment);
+
+    const commentInfo = await this.findOne(newComment.id, { select: ["id", "comment", "createdAt", "updatedAt"] });
+
+    return commentInfo;
+  }
+}
