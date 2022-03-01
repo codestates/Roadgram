93e321e5eecf1ad4de9c9f3ffbb8229c10214556
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2022-03-01 22:12:02 +0900

    Add routelistslice file

--- client/src/store/RouteListSlice.tsx
+++ client/src/store/RouteListSlice.tsx
@@ -30,18 +30,25 @@ const routeListSlice = createSlice({
       if (state.routeList.length < 5) {
         state.routeList.push(payload)
       } else {
-        alert('경로를 더이상 추가할 수 없습니다')
+        alert('경로는 5개까지 추가 가능합니다.')
       }
-    },
-    deleteRouteList: (state: routes) => {
-      state.routeList.pop()
     },
     setImages: (state: routes, { payload }: PayloadAction<any>) => {
       state.routeList[payload.idx].imageSrc = payload.src;
+    },
+    deleteMakerSelected: (state: routes, { payload }: PayloadAction<any>) => {
+      state.routeList = payload;
+    },
+    dragMakerChange: (state: routes, { payload }: PayloadAction<any>) => {
+      const dragIdx = payload[0].idx;
+      const enterIdx = payload[1].idx;
+      // 드래그 한 값을 서로 반대로 넣어줌
+      state.routeList[dragIdx] = payload[1].value;
+      state.routeList[enterIdx] = payload[0].value;
     },
     resetRouteList: () => initialState
   },
 })
 
-export const { getRouteList, addRouteList, deleteRouteList, setImages, resetRouteList } = routeListSlice.actions
+export const { getRouteList, addRouteList, setImages, deleteMakerSelected, dragMakerChange, resetRouteList } = routeListSlice.actions
 export default routeListSlice.reducer
