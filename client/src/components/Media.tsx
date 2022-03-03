931b74a624f4eb459c645a0aef1ebaf657e6e11f
박종범 <82630476+pjb642@users.noreply.github.com>
2022-03-03 11:20:27 +0900

    Create Map Component

--- client/src/components/Media.tsx
+++ client/src/components/Media.tsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react'
-import '../styles/components/_media.scss'
 import { useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight, faCircle } from '@fortawesome/free-solid-svg-icons'
 import { RootState } from '..'
 
+declare global {
+  interface Window {
+    kakao: any
+  }
+}
+
+// 지도의 범위를 설정하는 함수를 불러온다
+const bounds = new window.kakao.maps.LatLngBounds()
+
 function Media() {
   const { articleInfo } = useSelector((state: RootState) => state.articleDetails)
-  console.log('오고있니?', articleInfo)
 
   const [current, setCurrent] = useState(0)
   const [imageLength, setImageLength] = useState(0)
+  const [isMap, setIsMap] = useState(false)
 
   useEffect(() => {
     if (articleInfo.roads) {
@@ -38,31 +46,33 @@ function Media() {
   }
 
   return (
-    <div className="slider">
-      <FontAwesomeIcon className="angleLeft" icon={faAngleLeft} onClick={prevSlide} />
-      <FontAwesomeIcon className="angleRight" icon={faAngleRight} onClick={nextSlide} />
-      <div className="dots">
-        {articleInfo.roads ? (
-          articleInfo.roads.map(image => {
-            return (
-              <FontAwesomeIcon
-                className={articleInfo.roads.indexOf(image) === current ? 'dot-active' : 'dot'}
-                icon={faCircle}
-                onClick={() => moveDot(articleInfo.roads.indexOf(image))}
-              />
-            )
-          })
-        ) : (
-          <div>등록된 이미지가 없습니다.</div>
-        )}
+    <div>
+      <div className="slider">
+        <FontAwesomeIcon className="angleLeft" icon={faAngleLeft} onClick={prevSlide} />
+        <FontAwesomeIcon className="angleRight" icon={faAngleRight} onClick={nextSlide} />
+        <div className="dots">
+          {articleInfo.roads ? (
+            articleInfo.roads.map(image => {
+              return (
+                <FontAwesomeIcon
+                  className={articleInfo.roads.indexOf(image) === current ? 'dot-active' : 'dot'}
+                  icon={faCircle}
+                  onClick={() => moveDot(articleInfo.roads.indexOf(image))}
+                />
+              )
+            })
+          ) : (
+            <div>등록된 이미지가 없습니다.</div>
+          )}
+        </div>
+        {articleInfo.roads.map(image => {
+          return (
+            <div className={articleInfo.roads.indexOf(image) === current ? 'slide active' : 'slide'}>
+              {articleInfo.roads.indexOf(image) === current && <img className="image" alt="img" src={image.imageSrc} />}
+            </div>
+          )
+        })}
       </div>
-      {articleInfo.roads.map(image => {
-        return (
-          <div className={articleInfo.roads.indexOf(image) === current ? 'slide active' : 'slide'}>
-            {articleInfo.roads.indexOf(image) === current && <img className="image" alt="img" src={image.imageSrc} />}
-          </div>
-        )
-      })}
     </div>
   )
 }
