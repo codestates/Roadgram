fd685b490df5ea0d6cf552ac378d2a567f58be18
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-03-07 11:36:34 +0900

    Fix user entity 유저 엔티티에 업데이트 시 비밀번호 해쉬화 안하던 오류 수정

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -5,6 +5,7 @@ import { Comments } from '../../comments/entities/comments.entity';
 import * as bcrypt from 'bcrypt';
 import {
   BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
@@ -69,6 +70,7 @@ export class User {
   comments?: Comment[];
 
   @BeforeInsert()
+  @BeforeUpdate()
   async hashPassword(password: string) {
     if(this.loginMethod===0){
       const salt = await bcrypt.genSalt();
