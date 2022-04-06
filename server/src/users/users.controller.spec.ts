cd6e7158fd758cd6b80046be5d97fa338699b85c
KwonJin Jung <91672778+qwp0905@users.noreply.github.com>
2022-04-06 12:55:16 +0900

    Update users.controller.spec.ts
    테스트 문구를 컨트롤러에 함수명으로 변경

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -34,56 +34,52 @@ describe('UsersController', () => {
     service = module.get<UsersService>(UsersService)
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  it('1. 로그인 테스트', () => {
+  it('1. login 테스트', () => {
     controller.login({ email: '', password: '' });
     expect(service.login).toBeCalledTimes(1);
   });
 
-  it('2. 로그아웃 테스트', () => {
+  it('2. logout 테스트', () => {
     controller.logout({ loginMethod: 1, user: 1 });
     expect(service.logout).toBeCalledTimes(1);
   });
 
-  it('3. 회원가입 테스트', () => {
+  it('3. signup 테스트', () => {
     controller.signup({ email: '', password: '', nickname: '' })
     expect(service.signup).toBeCalledTimes(1);
   })
 
-  it('4. 이메일 중복 테스트', () => {
+  it('4. checkEmail 테스트', () => {
     controller.checkEmail({ email: '' })
     expect(service.checkEmail).toBeCalledTimes(1);
   })
 
-  it('5. 닉네임 중복 테스트', () => {
+  it('5. checkNickname 테스트', () => {
     controller.checkNickname({ nickname: '' })
     expect(service.checkNickname).toBeCalledTimes(1);
   })
 
-  it('6. 카카오 로그인 테스트', () => {
+  it('6. getToken 테스트', () => {
     controller.getToken({ code: '' })
     expect(service.getTokenKakao).toBeCalledTimes(1);
   })
 
-  it('7. 회원정보 수정 테스트', () => {
+  it('7. modifyProfile 테스트', () => {
     controller.modifyProfile({ user: 1, loginMethod: 1 })
     expect(service.modifyUser).toBeCalledTimes(1);
   })
 
-  it('8. 회원탈퇴 테스트', () => {
+  it('8. deleteUser 테스트', () => {
     controller.deleteUser({ user: 1, loginMethod: 1 })
     expect(service.deleteUser).toBeCalledTimes(1);
   })
 
-  it('9. 마이페이지 테스트', () => {
+  it('9. getUserInfo 테스트', () => {
     controller.getUserInfo(1, 2)
     expect(service.getMypage).toBeCalledTimes(1);
   })
 
-  it('10. 토큰 연장 테스트', () => {
+  it('10. refreshAccessToken 테스트', () => {
     controller.refreshAccessToken({ user: 1, loginMethod: 1 }, { authorization: '' })
     expect(service.refreshAccessToken).toBeCalledTimes(1);
   })
