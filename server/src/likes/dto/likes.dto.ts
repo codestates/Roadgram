f4f9a0f329a6786497f1a7a34e4d7af9410ad18e
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2022-04-10 14:31:10 +0900

    Add swagger api to like api

--- server/src/likes/dto/likes.dto.ts
+++ server/src/likes/dto/likes.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsNotEmpty } from 'class-validator';
 
 export class LikesDto {
+  @ApiProperty({description: "로그인 방식", example: 1})
   @IsNotEmpty()
   @IsNumber()
   loginMethod: number;
 
+  @ApiProperty({description: "유저ID", example: 5})
   @IsNotEmpty()
   @IsNumber()
   user: number;
 
+  @ApiProperty({description: "게시물ID",example: 15})
   @IsNotEmpty()
   @IsNumber()
   articleId: number;
--- server/src/likes/likes.controller.ts
+++ server/src/likes/likes.controller.ts
@@ -2,7 +2,8 @@ import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesDto } from './dto/likes.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LikeUnLike404Response, LikeUnLikeResponse } from './response/likeUnlike.response';
 
 @ApiTags('Likes')
 @Controller('likes')
@@ -10,8 +11,11 @@ export class LikesController {
   constructor(private likesService: LikesService) {}
 
     @Post()
+    @ApiOperation({summary: "좋아요 / 좋아요 취소 API", description: "좋아요 / 좋아요 취소 결과"})
+    @ApiCreatedResponse({description: "좋아요 / 좋아요 취소가 정상적으로 처리됨", type: LikeUnLikeResponse})
+    @ApiNotFoundResponse({description: "해당 게시물을 찾을 수 없음", type: LikeUnLike404Response})
+    @ApiBearerAuth()
     @UseGuards(AuthGuard)
-    @HttpCode(200)
     likeUnlike(
       @Body() likesDto: LikesDto,
     ): Promise<object> {
