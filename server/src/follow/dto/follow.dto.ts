a950336d917cbc5a42b290877d98fe1db823e5d1
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2022-04-10 14:33:28 +0900

    Fix follow dto api property

--- server/src/follow/dto/follow.dto.ts
+++ server/src/follow/dto/follow.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsNotEmpty } from 'class-validator';
 
 export class FollowDto {
+  @ApiProperty({description: "로그인 방식", example: 1})
   @IsNotEmpty()
   @IsNumber()
   loginMethod: number;
 
+  @ApiProperty({description: "유저ID", example: 2})
   @IsNotEmpty()
   @IsNumber()
   user: number;
 
+  @ApiProperty({description: "팔로잉 유저ID", example: 5})
   @IsNotEmpty()
   @IsNumber()
   followingUserId: number;
